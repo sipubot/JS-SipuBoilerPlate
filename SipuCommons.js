3a6b733b40e716e90747703f2331ffb0b43a0b14
sipubot <ddasik00@naver.com>
2016-01-12 17:59:20 +0900

    base work

--- SipuCommons.js
+++ SipuCommons.js
@@ -1,13 +1,17 @@
 var SipuCommons = (function(SipuCommons, undefined) {
   "use strict";
   SipuCommons.init = {
-    VALUE : ""
+    VALUE : "this is basic structure"
   };
 
-  
+  function getEvent() {
+    console.dir(window);
+  }
 
   SipuCommons.start = function() {
-
+    console.log(SipuCommons.init.VALUE);
+    getEvent();
   };
+  return SipuCommons;
 })(window.SipuCommons || {});
 SipuCommons.start();
